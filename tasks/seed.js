4e88ca0ddb41a1bc24e6e4df7d7e6c46a732c8f8
zz-chen-s <ckahome2007@gmail.com>
2017-12-11 22:11:56 -0500

    added multiple roles and quizzes in seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -21,6 +21,18 @@ let newUser = new User({
     }]
 });
 
+let newStudent = new User({
+    _id: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b8964",
+    role: "student",
+    sessionId: "b3988882-627f-4c59-8d5d-54b7a43b030e",
+    hashPassword: "123456",
+    profile: {
+        name: "John",
+        campusId: "234567"
+    },
+    grades: []
+});
+
 let newProfessor = new User({
     _id: "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b8745",
     role: "teacher",
@@ -50,6 +62,22 @@ let newQuiz = new Quiz({
     ]
 });
 
+let newQuiz2 = new Quiz({
+    _id: "b3988882-627f-5d60",
+    name: "Final",
+    totalScore: 100,
+    questions: [
+        {
+            "id": "1",
+            "content": "Have you been to Canada?"
+        },
+        {
+            "id": "2",
+            "content": "Have you been to Mexico?"
+        }
+    ]
+});
+
 mongoose.connection.dropDatabase();
 
 User.createUser(newUser, function (err, res) {
@@ -57,7 +85,16 @@ User.createUser(newUser, function (err, res) {
         console.log("Error:" + err);
     }
     else {
-        console.log("Create Student:" + res);
+        console.log("Create Student 1:" + res);
+    }
+});
+
+User.createUser(newStudent, function (err, res) {
+    if (err) {
+        console.log("Error:" + err);
+    }
+    else {
+        console.log("Create Student 2:" + res);
     }
 });
 
@@ -80,3 +117,14 @@ Quiz.createQuiz(newQuiz, function (err, res) {
     }
 
 });
+
+Quiz.createQuiz(newQuiz2, function (err, res) {
+    
+        if (err) {
+            console.log("Error:" + err);
+        }
+        else {
+            console.log("Res2:" + res);
+        }
+    
+    });
